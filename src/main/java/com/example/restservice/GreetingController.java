26063356e21b62d4b480e4c60449bdff68f70d42
unknown <chaturgaurav35@gmail.com>
2025-03-03 17:03:03 +0530

    correction in controller

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -8,13 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	@GetMapping
+	String greeting(){
+		return "Hello World";
 	}
-
 }
