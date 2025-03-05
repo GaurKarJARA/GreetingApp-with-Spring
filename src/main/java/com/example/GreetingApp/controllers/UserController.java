af1ca94c6e5d3284c524cfe6d18338d4c344ade7
unknown <chaturgaurav35@gmail.com>
2025-03-05 19:54:13 +0530

    User Login added

--- src/main/java/com/example/GreetingApp/controllers/UserController.java
+++ src/main/java/com/example/GreetingApp/controllers/UserController.java
@@ -1,7 +1,7 @@
 package com.example.GreetingApp.controllers;
 import com.example.GreetingApp.dto.AuthUserDTO;
-import com.example.GreetingApp.service.AuthenticationService;
-import com.example.GreetingApp.service.EmailService;
+import com.example.GreetingApp.service.*;
+import com.example.GreetingApp.dto.LoginDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,18 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     EmailService emailService;
     AuthenticationService authenticationService;
+    JwtTokenService jwtTokenService;
 
-    public UserController(EmailService emailService, AuthenticationService authenticationService) {
+    public UserController(EmailService emailService, AuthenticationService authenticationService,JwtTokenService jwtTokenService ) {
         this.emailService = emailService;
         this.authenticationService = authenticationService;
+        this.jwtTokenService = jwtTokenService;
     }
 
-    //UC9 --> For Registration of a user
+    //UC-UserRegistration --> For Registration of a user
     @PostMapping(path = "/register")
     public String register(@RequestBody AuthUserDTO user){
         return authenticationService.register(user);
     }
 
-
+    //UC-login --> For User Login
+    @PostMapping(path ="/login")
+    public String login(@RequestBody LoginDTO user){
+        return authenticationService.login(user);
+    }
 
 }
--- src/main/java/com/example/GreetingApp/service/AuthenticationService.java
+++ src/main/java/com/example/GreetingApp/service/AuthenticationService.java
@@ -1,5 +1,5 @@
 package com.example.GreetingApp.service;
-
+import com.example.GreetingApp.dto.LoginDTO;
 import com.example.GreetingApp.dto.AuthUserDTO;
 import com.example.GreetingApp.models.AuthUser;
 import com.example.GreetingApp.repository.UserRepository;
@@ -14,11 +14,12 @@ import java.util.stream.Collectors;
 
         UserRepository userRepository;
         EmailService emailService;
+        JwtTokenService jwtTokenService;
 
-
-        public AuthenticationService(UserRepository userRepository, EmailService emailService) {
+        public AuthenticationService(UserRepository userRepository, EmailService emailService,JwtTokenService jwtTokenService) {
             this.userRepository = userRepository;
             this.emailService = emailService;
+            this.jwtTokenService = jwtTokenService;
         }
 
         public String register(AuthUserDTO user){
@@ -46,6 +47,31 @@ import java.util.stream.Collectors;
             emailService.sendEmail(user.getEmail(), "Regitration Status", user.getFirstName()+" you are registered!");
 
             return "user registered";
+        }
+        public String login(LoginDTO user){
+
+            List<AuthUser> l1 = userRepository.findAll().stream().filter(authuser -> authuser.getEmail().equals(user.getEmail())).collect(Collectors.toList());
+            if(l1.size() == 0)
+                return "User not registered";
+
+            AuthUser foundUser = l1.get(0);
+
+            //matching the stored hashed password with the password provided by user
+            BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+
+            if(!bcrypt.matches(user.getPassword(), foundUser.getHashPass()))
+                return "Invalid password";
+
+            //creating Jwt Token
+            String token = jwtTokenService.createToken(foundUser.getId());
+
+            //setting token for user login
+            foundUser.setToken(token);
+
+            //saving the current status of user in database
+            userRepository.save(foundUser);
+
+            return "user logged in"+"\ntoken : "+token;
         }
 
     }
